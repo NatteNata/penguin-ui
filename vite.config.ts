0ad1338bcb660ee1d100d58a993f63ce622533ba
NatteNata <natte@yandex.ru>
2025-04-21 20:44:46 +0300

    chore: library mode set-up

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	build: {
+		lib: {
+			entry: 'src/index',
+			name: 'penguin-ui',
+			formats: ['es', 'cjs', 'umd'],
+			fileName: format => `penguin-ui.${format}.js`,
+		},
+	},
 })
