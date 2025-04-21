ef67377a41b5dc02360264e63f0c94a374515163
NatteNata <natte@yandex.ru>
2025-04-21 21:00:35 +0300

    chore: final lib set-up

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,7 +3,7 @@ export const Button = (props: ComponentPropsWithoutRef<'button'>) => {
 	return (
 		<button
 			type='button'
-			className='rounded bg-pink-400 px-4 py-2 text-white text-xl'
+			className='rounded bg-emerald-800 px-4 py-2 text-white text-xl'
 			{...props}
 		>
 			Test button
