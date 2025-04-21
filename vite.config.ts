ef716fb96206028d710f6a6f0ba489bdfbd94370
NatteNata <natte@yandex.ru>
2025-04-21 20:49:10 +0300

    chore: dts set-up

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		dts({
+			tsconfigPath: './tsconfig.app.json',
+			exclude: ['**/*.stories.ts', '**/*.test.ts'],
+		}),
+		tailwindcss(),
+	],
 	build: {
 		lib: {
 			entry: 'src/index',
